0a6b9bb6605aecec66d4d5cedb84bb5169245558
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-03 13:26:14 +0700

    Backdrop Carousel

---
+++ BackdropCarousel/BackdropCarousel/BackdropCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackdropCarouselApp.swift
+//  BackdropCarousel
+//
+//  Created by Adrian Suryo Abiyoga on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BackdropCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackdropCarousel/BackdropCarousel/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BackdropCarousel
+//
+//  Created by Adrian Suryo Abiyoga on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+            .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
